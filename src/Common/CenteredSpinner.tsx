d8676378996a3cc0ae635ae5599c9da181fb34f4
Ben C <bwc9876@gmail.com>
2022-07-03 00:51:36 -0400

    Lint

--- src/Common/CenteredSpinner.tsx
+++ src/Common/CenteredSpinner.tsx
@@ -1,3 +1,4 @@
+
 function CenteredSpinner() {
     return <div className={"h-100 w-100 d-flex justify-content-center align-items-center"}>
         <img className="nh-spinner" height="192" width="192" src="src/Common/Spinner/spinner_image.png"
